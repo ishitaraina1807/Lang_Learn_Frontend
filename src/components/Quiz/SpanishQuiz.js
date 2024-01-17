b35b1b14a76f9d71a9f1193ea9b2b60397429413
ishitaraina1807 <ishitaraina18@gmail.com>
2024-01-17 19:11:28 +0530

    profile

--- src/components/Quiz/SpanishQuiz.js
+++ src/components/Quiz/SpanishQuiz.js
@@ -167,7 +167,14 @@ const SpanishQuiz = ({ quizData }) => {
                         )}
                         {showResult && <ResultComponent score={score} />}
                     </div>
-
+                    <Link  to="/profile" className="flex items-center justify-center hover:scale-110 text-[#152039] mx-auto hover:text-[#CFD724] transition duration-300 hover:cursor-pointer">
+                    <div
+                        className="w-10 h-10"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"> <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" /></svg>
+                    </div>
+                    <p>View your Profile</p> 
+                    </Link>
                 </div>
             )}
 
